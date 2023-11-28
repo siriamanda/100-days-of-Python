7b00f3c65e446e30bf84e695d03ec19a7f3f6ab4
Siri Amanda <sraaf@babbel.com>
2023-11-28 17:56:03 +0100

    Day 20 snake game

---
+++ day20-snake-game/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.squares = []
+        self.create_snake()
+        self.head = self.squares[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_square = Turtle(shape="square")
+            new_square.color("white")
+            new_square.penup()
+            new_square.goto(position)
+            self.squares.append(new_square)
+
+    def move(self):
+        for square_num in range(len(self.squares) - 1, 0, -1):
+            new_x = self.squares[square_num - 1].xcor()
+            new_y = self.squares[square_num - 1].ycor()
+            self.squares[square_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
