261da6b71531bffd20b55b89a3a9c13e6b432f17
Siri Amanda <sraaf@babbel.com>
2024-01-18 08:27:55 +0100

    Adding functionality saving to file for password manager

--- day29-password-manager/main.py
+++ day29-password-manager/main.py
@@ -1,14 +1,34 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oups", message="Please don't leave any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+                                                  f"\nPassword: {password} \n Is it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:    # We use append (a) since we want to write to the file without overwriting
+                file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 password_logo = PhotoImage(file="logo.png")
@@ -20,7 +40,7 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-email_label = Label(text="Email/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
 password_label = Label(text="Password:")
@@ -30,19 +50,21 @@ password_label.grid(column=0, row=3)
 
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()    # Add cursor in entry when opening program
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "siriamandaraaf@gmail.com")
 
 password_entry = Entry(width=18)
 password_entry.grid(column=1, row=3, columnspan=1)
 
 # Buttons
 
-generate_password_button = Button(text="Generate Password", width=13)
+generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=33)
+add_button = Button(text="Add", width=33, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
