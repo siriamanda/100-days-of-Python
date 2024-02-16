8322672de78aef767cc1e31b240ff8a61e6a343d
Siri Amanda <sraaf@babbel.com>
2024-02-16 09:40:56 +0100

    Flipping card using window.after()

--- day31-flash-card-project-start/main.py
+++ day31-flash-card-project-start/main.py
@@ -6,15 +6,27 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data_file = pd.read_csv("data/french_words.csv")
 word_dict_list = data_file.to_dict(orient="records")
+random_selection = {}
 
 # ---------------------------- FUNCTIONS ------------------------------- #
 
 def next_card():
+    global random_selection, flip_timer
+    window.after_cancel(flip_timer)
     random_selection = random.choice(word_dict_list)
     french_word = random_selection["French"]
+    canvas.itemconfig(word_label, text=french_word, fill="black")
+    canvas.itemconfig(language_label, text="French", fill="black")
+    canvas.itemconfig(card_background, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
     english_word = random_selection["English"]
-    canvas.itemconfig(word_label, text=french_word)
-    canvas.itemconfig(language_label, text="French")
+    canvas.itemconfig(language_label, text="English", fill="white")
+    canvas.itemconfig(word_label, text=english_word, fill="white")
+    canvas.itemconfig(card_background, image=card_back)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -23,13 +35,15 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 card_front = PhotoImage(file="images/card_front.png")
 card_back = PhotoImage(file="images/card_back.png")
 right = PhotoImage(file="images/right.png")
 wrong = PhotoImage(file="images/wrong.png")
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, background=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=card_front)
+card_background = canvas.create_image(400, 263, image=card_front)
 canvas.grid(column=0, row=0, columnspan=2)
 
 language_label = canvas.create_text(400, 150, text="", font=('Ariel', 40, 'italic'))
@@ -43,6 +57,7 @@ right_button.grid(column=1, row=1)
 # ---------------------------- FLASH CARD ------------------------------- #
 
 next_card()
+
 
 # random_word = data_file.French.sample()
 # print(random_word)
