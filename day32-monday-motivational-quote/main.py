df5b797ce6998f021ba601f3a5996b21bc9a2a3e
Siri Amanda <sraaf@babbel.com>
2024-02-27 09:43:36 +0100

    Adding starting file for birthday wisher

---
+++ day32-monday-motivational-quote/main.py
@@ -0,0 +1,58 @@
+import datetime as dt
+import random
+import smtplib
+
+my_email = 'ziriamanda@gmail.com'
+password = 'qtnydxglyshyfcex'
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+if day_of_week == 0:
+
+    with open("quotes.txt", "r") as quotes:
+        quotes_list = quotes.readlines()
+        random_quote = random.choice(quotes_list)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()     # Secure connection and encrypt email
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="ziriamanda@outlook.com",
+            msg=f"Subject:Monday Motivation \n\n {random_quote}")
+
+
+
+
+
+
+
+
+# import smtplib
+
+# Outlook smtp smtp.live.com
+
+# my_email = 'ziriamanda@gmail.com'
+# password = 'qtnydxglyshyfcex'
+
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()     # Secure connection and encrypt email
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="ziriamanda@outlook.com",
+#                        msg="Subject:Hello \n\n This is the body of email")
+#     connection.close()
+
+# import datetime as dt
+
+# now = dt.datetime.now()
+# year = now.year
+# print(year)
+
+# print(date_of_birth)
+
+
+
+
